8a0d438a0b23b1510224069afcf3cc4fa26ad6d8
nemuigaki <2094176918@qq.com>
2023-12-10 22:34:59 +0900

    feat: 基于责任链模式实现消息发直送至MQ逻辑

---
+++ DoPush-Server/push-domain/src/main/java/com/sadalsuud/push/domain/gateway/MessageTemplateDao.java
@@ -0,0 +1,13 @@
+package com.sadalsuud.push.domain.gateway;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author sadalsuud
+ * @Blog www.sadalsuud.cn
+ * @Date 10/12/2023
+ * @Package com.sadalsuud.push.domain.gateway
+ */
+public interface MessageTemplateDao {
+}
---
+++ DoPush-Server/push-domain/src/main/java/com/sadalsuud/push/domain/pipeline/MessageParam.java
@@ -0,0 +1,50 @@
+package com.sadalsuud.push.domain.pipeline;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.Map;
+
+/**
+ * @Description 消息参数
+ * @Author sadalsuud
+ * @Blog www.sadalsuud.cn
+ * @Date 10/12/2023
+ * @Package com.sadalsuud.push.domain.pipeline
+ */
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MessageParam {
+
+    /**
+     * 业务消息发送Id, 用于链路追踪, 若不存在, austin 则生成一个消息Id
+     */
+    private String bizId;
+
+    /**
+     * @Description: 接收者
+     * 多个用,逗号号分隔开
+     * 【不能大于100个】
+     * 必传
+     */
+    private String receiver;
+
+    /**
+     * @Description: 消息内容中的可变部分(占位符替换)
+     * 可选
+     */
+    private Map<String, String> variables;
+
+    /**
+     * @Description: 扩展参数
+     * 可选
+     */
+    private Map<String, String> extra;
+}
+
