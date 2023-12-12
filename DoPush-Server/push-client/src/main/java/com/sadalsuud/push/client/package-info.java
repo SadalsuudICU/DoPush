7936c2fae7ad484828ca0f9ccf5b8cb8fe7e5b87
nemuigaki <2094176918@qq.com>
2023-12-12 11:44:07 +0900

    docs: 为client模块添加模块说明

---
+++ DoPush-Server/push-client/src/main/java/com/sadalsuud/push/client/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @Description 客户端模块 用于定义接口api和客户端与服务端进行交互的DTO类
+ * @Author sadalsuud
+ * @Blog www.sadalsuud.cn
+ * @Date 12/12/2023
+ * @Package com.sadalsuud.push.client
+ */
+package com.sadalsuud.push.client;
