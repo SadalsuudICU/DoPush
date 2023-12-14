e83e1740179b76a160c603d2fc6b3edd948b6be9
nemuigaki <2094176918@qq.com>
2023-12-14 09:20:19 +0800

    feat: 添加线程池关闭机制和引入OK HTTP

---
+++ DoPush-Server/push-infrastructure/src/main/java/com/sadalsuud/push/infrastructure/corn/SupportThreadPoolConfig.java
@@ -0,0 +1,40 @@
+package com.sadalsuud.push.infrastructure.corn;
+
+import cn.hutool.core.thread.ExecutorBuilder;
+import com.sadalsuud.push.common.constant.ThreadPoolConstant;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description 线程池配置类
+ * @Author sadalsuud
+ * @Blog www.sadalsuud.cn
+ * @Date 14/12/2023
+ * @Package com.sadalsuud.push.domain.corn
+ */
+public class SupportThreadPoolConfig {
+
+    private SupportThreadPoolConfig() {
+    }
+
+    /**
+     * 业务：实现pending队列的单线程池
+     * 配置：核心线程可以被回收，当线程池无被引用且无核心线程数，应当被回收
+     * 动态线程池且被Spring管理：false
+     */
+    @SuppressWarnings("unchecked")
+    public static ExecutorService getPendingSingleThreadPool() {
+        return ExecutorBuilder.create()
+                .setCorePoolSize(ThreadPoolConstant.SINGLE_CORE_POOL_SIZE)
+                .setMaxPoolSize(ThreadPoolConstant.SINGLE_MAX_POOL_SIZE)
+                .setWorkQueue(new LinkedBlockingQueue(ThreadPoolConstant.BIG_QUEUE_SIZE))
+                .setHandler(new ThreadPoolExecutor.CallerRunsPolicy())
+                .setAllowCoreThreadTimeOut(true)
+                .setKeepAliveTime(ThreadPoolConstant.SMALL_KEEP_LIVE_TIME, TimeUnit.SECONDS)
+                .build();
+    }
+
+}
