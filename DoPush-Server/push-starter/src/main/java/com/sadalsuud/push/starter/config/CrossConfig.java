b8b11d52b53024f36a670c3046e0a59ea5340e50
nemuigaki <2094176918@qq.com>
2023-12-19 14:07:57 +0800

    feat: 添加json通用配置和跨域配置

---
+++ DoPush-Server/push-starter/src/main/java/com/sadalsuud/push/starter/config/CrossConfig.java
@@ -0,0 +1,25 @@
+package com.sadalsuud.push.starter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description 跨域配置
+ * @Author sadalsuud
+ * @Blog www.sadalsuud.cn
+ * @Date 19/12/2023
+ * @Package com.sadalsuud.push.starter.config
+ */
+@Configuration
+public class CrossConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
