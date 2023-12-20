8bb7139dc5ba6d3b8281f2d65969b4e40c757416
nemuigaki <2094176918@qq.com>
2023-12-20 09:58:21 +0800

    docs: 为stream模块添加package info

---
+++ DoPush-Server/push-stream/src/main/java/com/sadalsuud/push/stream/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @Description 基于kafka、redis和flink的实时数据流实现数据清洗
+ * @Author sadalsuud
+ * @Blog www.sadalsuud.cn
+ * @Date 20/12/2023
+ * @Package com.sadalsuud.push.stream
+ */
+package com.sadalsuud.push.stream;
