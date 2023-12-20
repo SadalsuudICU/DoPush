2708fdeedb04f7dcc69e125332d6afb21a7f7835
nemuigaki <2094176918@qq.com>
2023-12-20 09:52:59 +0800

    fix: 解决无法直接使用Resource注入redisTemplate

--- DoPush-Server/push-domain/src/main/java/com/sadalsuud/push/domain/support/AccessTokenUtils.java
+++ DoPush-Server/push-domain/src/main/java/com/sadalsuud/push/domain/support/AccessTokenUtils.java
@@ -30,7 +30,7 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class AccessTokenUtils {
 
-    @Resource
+    @Resource(name = "stringRedisTemplate")
     private StringRedisTemplate redisTemplate;
 
     /**
