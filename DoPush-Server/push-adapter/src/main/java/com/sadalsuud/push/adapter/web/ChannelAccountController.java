1bf60f828009e8bde9c97af2a770bf36a157d316
nemuigaki <2094176918@qq.com>
2023-12-24 00:09:19 +0800

    feat: 新增渠道管理接口

--- DoPush-Server/push-adapter/src/main/java/com/sadalsuud/push/adapter/web/ChannelAccountController.java
+++ DoPush-Server/push-adapter/src/main/java/com/sadalsuud/push/adapter/web/ChannelAccountController.java
@@ -4,6 +4,7 @@ import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.text.StrPool;
 import com.sadalsuud.push.adapter.facade.annotation.DoPushAspect;
 import com.sadalsuud.push.adapter.facade.annotation.DoPushResult;
+import com.sadalsuud.push.adapter.facade.exception.CommonException;
 import com.sadalsuud.push.client.api.ChannelAccountService;
 import com.sadalsuud.push.common.constant.DoPushConstant;
 import com.sadalsuud.push.common.enums.RespStatusEnum;
@@ -12,7 +13,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -37,49 +37,53 @@ public class ChannelAccountController {
 
     private final ChannelAccountService channelAccountService;
 
-    //@Autowired
     //private LoginUtils loginUtils;
 
     /**
      * 如果Id存在，则修改
      * 如果Id不存在，则保存
      */
-    //@PostMapping("/save")
-    //@ApiOperation("/保存数据")
-    //public ChannelAccount saveOrUpdate(@RequestBody ChannelAccount channelAccount) {
-    //    if (loginUtils.needLogin() && CharSequenceUtil.isBlank(channelAccount.getCreator())) {
-    //        throw new CommonException(RespStatusEnum.NO_LOGIN.getCode(), RespStatusEnum.NO_LOGIN.getMsg());
-    //    }
-    //    channelAccount.setCreator(CharSequenceUtil.isBlank(channelAccount.getCreator()) ? AustinConstant.DEFAULT_CREATOR : channelAccount.getCreator());
-    //
-    //    return channelAccountService.save(channelAccount);
-    //}
-    //
-    ///**
-    // * 根据渠道标识查询渠道账号相关的信息
-    // */
-    //@GetMapping("/queryByChannelType")
-    //@ApiOperation("/根据渠道标识查询相关的记录")
-    //public List<CommonAmisVo> query(Integer channelType, String creator) {
-    //    if (loginUtils.needLogin() && CharSequenceUtil.isBlank(creator)) {
-    //        throw new CommonException(RespStatusEnum.NO_LOGIN.getCode(), RespStatusEnum.NO_LOGIN.getMsg());
-    //    }
-    //    creator = CharSequenceUtil.isBlank(creator) ? AustinConstant.DEFAULT_CREATOR : creator;
-    //
-    //    List<ChannelAccount> channelAccounts = channelAccountService.queryByChannelType(channelType, creator);
-    //    return Convert4Amis.getChannelAccountVo(channelAccounts, channelType);
-    //}
-    //
+    @PostMapping("/save")
+    @ApiOperation("/保存数据")
+    public ChannelAccount saveOrUpdate(@RequestBody ChannelAccount channelAccount) {
+        if (
+                //loginUtils.needLogin() &&
+                        CharSequenceUtil.isBlank(channelAccount.getCreator())) {
+            throw new CommonException(RespStatusEnum.NO_LOGIN.getCode(), RespStatusEnum.NO_LOGIN.getMsg());
+        }
+        channelAccount.setCreator(CharSequenceUtil.isBlank(channelAccount.getCreator()) ? DoPushConstant.DEFAULT_CREATOR : channelAccount.getCreator());
+
+        return channelAccountService.save(channelAccount);
+    }
+
+    /**
+     * 根据渠道标识查询渠道账号相关的信息
+     */
+    @GetMapping("/queryByChannelType")
+    @ApiOperation("/根据渠道标识查询相关的记录")
+    public List<ChannelAccount> query(Integer channelType, String creator) {
+        if (
+                //loginUtils.needLogin() &&
+                CharSequenceUtil.isBlank(creator)) {
+            throw new CommonException(RespStatusEnum.NO_LOGIN.getCode(), RespStatusEnum.NO_LOGIN.getMsg());
+        }
+        creator = CharSequenceUtil.isBlank(creator) ? DoPushConstant.DEFAULT_CREATOR : creator;
+
+        return channelAccountService.queryByChannelType(channelType, creator);
+    }
+
     /**
      * 所有的渠道账号信息
      */
     @GetMapping("/list")
     @ApiOperation("/渠道账号列表信息")
     public List<ChannelAccount> list(String creator) {
-        //if (loginUtils.needLogin() && CharSequenceUtil.isBlank(creator)) {
-        //    throw new CommonException(RespStatusEnum.NO_LOGIN.getCode(), RespStatusEnum.NO_LOGIN.getMsg());
-        //
-        //}
+        if (
+                //loginUtils.needLogin() &&
+                        CharSequenceUtil.isBlank(creator)) {
+            throw new CommonException(RespStatusEnum.NO_LOGIN.getCode(), RespStatusEnum.NO_LOGIN.getMsg());
+
+        }
         creator = CharSequenceUtil.isBlank(creator) ? DoPushConstant.DEFAULT_CREATOR : creator;
 
         return channelAccountService.list(creator);
