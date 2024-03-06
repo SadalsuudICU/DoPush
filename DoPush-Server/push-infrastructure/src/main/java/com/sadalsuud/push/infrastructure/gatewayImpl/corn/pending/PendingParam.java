45b51ef2a366a979792e049f3cfc65e7138fb786
nemuigaki <2094176918@qq.com>
2024-03-06 16:03:39 +0800

    feat: 接入 xxl-job 分布式定时框架

---
+++ DoPush-Server/push-infrastructure/src/main/java/com/sadalsuud/push/infrastructure/gatewayImpl/corn/pending/PendingParam.java
@@ -0,0 +1,43 @@
+package com.sadalsuud.push.infrastructure.gatewayImpl.corn.pending;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * @Description pending初始化参数
+ * @Author sadalsuud
+ * @Blog www.sadalsuud.cn
+ * @Date 2024/3/6
+ * @Project DoPush-Server
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class PendingParam<T> {
+
+    /**
+     * 消费线程池实例【必填】
+     */
+    protected ExecutorService executorService;
+    /**
+     * 阻塞队列实现类【必填】
+     */
+    private BlockingQueue<T> queue;
+    /**
+     * batch 触发执行的数量阈值【必填】
+     */
+    private Integer numThreshold;
+    /**
+     * batch 触发执行的时间阈值，单位毫秒【必填】
+     */
+    private Long timeThreshold;
+
+}
